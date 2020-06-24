5f3ce4eb211a3fc04334fcaa3c4677a15d99a154
alexistj <josepa2@rpi.edu>
2020-06-24 14:27:26 -0500

    training WIP

--- train.py
+++ train.py
@@ -9,6 +9,8 @@ from PIL import Image
 import requests
 from io import BytesIO
 import time
+from past.builtins import xrange
+
 
 
 with open('config.json') as config_file:
@@ -24,16 +26,23 @@ trainer = CustomVisionTrainingClient(data["endpoint"], credentials)
 publish_iteration_name = "detectModel"
 projects = trainer.get_projects()
 prediction_resource_id = data['prediction_resource_id']
-
+tagged_images_with_regions = []
+vehicle_regions = defaultdict(list)
 for p in projects:
     if p.name == data["project_name"]:
         project = trainer.get_project(p.id)
         tags = trainer.get_tags(project.id)
+        print('Project Found')
     
+
+
+
+
+print('Loading locations')
 with open(csv_file, 'r',encoding='utf-8' ) as csv_f:
     reader = csv.reader(csv_f)
     line_count = 0
-    vehicle_regions = defaultdict(list)
+    
   
     d= []
 
@@ -48,31 +57,37 @@ with open(csv_file, 'r',encoding='utf-8' ) as csv_f:
         d['url'] = row[1]
         vehicle_regions[row[2]].append(d)
     
-    tagged_images_with_regions = []
+    
 
 
-
+    print('Generating regions')
     for image in vehicle_regions:
-   
-        response = requests.get(vehicle_regions[image][0]['url'])
+        url = vehicle_regions[image][0]['url']
+        try:
+            response = requests.get(url)
+            print('recieved')
+        except:
+            print("error retrieving image: "+url)
+            continue
         img = Image.open(BytesIO(response.content))
         regions =  []
         for region in vehicle_regions[image]:
-            #print(region)
-            regions.append([ Region(tag_id=tags[0], left=region['coordinates']['x'],top=region['coordinates']['y'],  width=region['coordinates']['w'],height=region['coordinates']['h']) ]  )   
+            print(region)
+            regions.append([ Region(tag_id=tags[0].id, left=region['coordinates']['x'],top=region['coordinates']['y'],  width=region['coordinates']['w'],height=region['coordinates']['h']) ]  )   
         tagged_images_with_regions.append(ImageFileCreateEntry(name=image, contents=img, regions=regions))
          
-batchsize = 64
-for i in xrange(0,len(tagged_images_with_regions), batchsize):
-    upload_result = trainer.create_images_from_files(project.id, images=tagged_images_with_regions[i:i+batchsize])
+
+
+print('Uploading images to trainer')
+
+for i in range(0,len(tagged_images_with_regions),64):
+    batch =  tagged_images_with_regions[i:i+64]
+    upload_result = trainer.create_images_from_files(project.id, images=batch)
     if not upload_result.is_batch_successful:
        print("Image batch upload failed.")
        for image in upload_result.images:
             print("Image status: ", image.status)
        exit(-1)
-
-
-
 
 print ("Training...")
 iteration = trainer.train_project(project.id)
